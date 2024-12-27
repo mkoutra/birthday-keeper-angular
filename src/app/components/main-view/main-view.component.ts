912b55acb1cb3d0b89fcab619a30305a462dab47
mkoutra <koutra98@hotmail.com>
2024-12-28 00:01:22 +0200

    Hide Administrator options for non-ADMINs

--- src/app/components/main-view/main-view.component.ts
+++ src/app/components/main-view/main-view.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SimpleTableComponent } from "../simple-table/simple-table.component";
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ListGroupMenuComponent } from '../list-group-menu/list-group-menu.component';
+import { UserService } from '../../shared/services/user.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-main-view',
   standalone: true,
-  imports: [SimpleTableComponent, NavbarComponent, ListGroupMenuComponent],
+  imports: [SimpleTableComponent, NavbarComponent, ListGroupMenuComponent, NgIf],
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.css'
 })
 export class MainViewComponent {
-
+    userService = inject(UserService)
+    user = this.userService.user
 }
