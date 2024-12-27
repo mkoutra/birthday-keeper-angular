fb089ac34c4056a56cd4f9647d0c23158d3f9309
mkoutra <koutra98@hotmail.com>
2024-12-27 20:14:12 +0200

    Insert dialog to delete option.
    - Insert dialog and create dialog component. It seems to work.
    - Modify main view and change the position and the order of the list group.
    - Change text of update friend window.

---
+++ src/app/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  standalone: true,
+  imports: [MatFormFieldModule,
+            MatInputModule,
+            FormsModule,
+            MatButtonModule,
+            MatDialogTitle,
+            MatDialogContent,
+            MatDialogActions,
+            MatDialogClose,],
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrl: './confirmation-dialog.component.css'
+})
+export class ConfirmationDialogComponent {
+  readonly dialogRef = inject(MatDialogRef<ConfirmationDialogComponent>);
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
--- src/app/components/simple-table/simple-table.component.ts
+++ src/app/components/simple-table/simple-table.component.ts
@@ -6,6 +6,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { Router, RouterLink } from '@angular/router';
 import { NgFor } from '@angular/common';
 
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+
 @Component({
   selector: 'app-simple-table',
   standalone: true,
@@ -16,7 +19,8 @@ import { NgFor } from '@angular/common';
 export class SimpleTableComponent {
     router = inject(Router);
     friends = friendsDemo;
-    
+    dialog = inject(MatDialog);
+
     sortedData: FriendResponse[];
 
     constructor() {
@@ -54,10 +58,21 @@ export class SimpleTableComponent {
         this.router.navigate(['/friends/edit', id]);
     }
 
-    onDeleteClicked(id:string) {
+    onDeleteClicked(id:string):void {
         console.log("Delete clicked for id: ", id);
+        const dialogRef = this.dialog.open(ConfirmationDialogComponent);
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result === 'confirm') {
+              this.deleteFriend(id); // Call the API to delete the friend
+            }
+        });
     }
 
+    deleteFriend(friendId: string): void {
+        // Call the backend API to delete the friend
+        console.log(`Deleting friend with ID: ${friendId} from the backend.`);
+      }
 }
 
 function compare(a: number | string, b: number | string, isAsc: boolean) {
