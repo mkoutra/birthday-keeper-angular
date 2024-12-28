bc93ddc1e1084dd108074d6ccfd60ad097e2c413
mkoutra <koutra98@hotmail.com>
2024-12-28 13:04:53 +0200

    Interceptor, Friend Service, Friend Insertion
    - Interceptor to add the JWT to the header was created,
    - Interfaces to handle friend requests and responses were created,
    - Friend service was created,
    - Add friend modified both in the template and in the TS. A useful
      addition was the ChageDetectorRef which updates Angular when a
      successful or not successfull response is returned.
    - I removed "Welcome" from navbar.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,10 +7,21 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 // Necessary for HttpClient
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
+// For interceptor
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { authInterceptorService } from './shared/interceptor/auth.interceptor';
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
               provideRouter(routes),
               provideAnimationsAsync(),
-              provideHttpClient(withInterceptorsFromDi()) // Necessary for HttpClient
+              provideHttpClient(withInterceptorsFromDi()),  // Necessary for HttpClient
+              
+              // The following are for Interceptors
+              {
+                provide: HTTP_INTERCEPTORS,
+                useClass: authInterceptorService,
+                multi:true
+              }
             ]
 };
--- src/app/components/add-friend/add-friend.component.ts
+++ src/app/components/add-friend/add-friend.component.ts
@@ -1,18 +1,21 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, ChangeDetectorRef } from '@angular/core';
 
-import { FormGroup, FormControl, ReactiveFormsModule, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
-import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
-import { UserService } from '../../shared/services/user.service';
 import { Router } from '@angular/router';
 import { NavbarComponent } from "../navbar/navbar.component";
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { pastDateValidator } from '../../shared/validator/past-date.validator';
+import { FriendService } from '../../shared/services/friend.service';
+import { FriendInsert } from '../../shared/interfaces/friend-insert';
+import { ErrorResponse } from '../../shared/interfaces/error-response';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-add-friend',
@@ -25,14 +28,16 @@ import { pastDateValidator } from '../../shared/validator/past-date.validator';
             MatTooltipModule,
             NavbarComponent,
             NavbarComponent,
-            MatDatepickerModule],
+            MatDatepickerModule,
+            NgIf],
   templateUrl: './add-friend.component.html',
   styleUrl: './add-friend.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddFriendComponent {
-    userService = inject(UserService);
+    friendService = inject(FriendService)
     router = inject(Router);
+    cdr = inject(ChangeDetectorRef);  // Used to notify Angular of the changes in the variables
 
     successfulInsertion: boolean = false;
     friendAlreadyExists: boolean = false;
@@ -54,39 +59,42 @@ export class AddFriendComponent {
     }
 
     onSubmit() {
-        console.log("Information obtained directly from the form: ", this.form.value);
-        console.log("Date Of Birth: ", this.form.get('dateOfBirth')?.value)
         let date: Date = this.form.get('dateOfBirth')?.value as unknown as Date
 
-        console.log(this.createValidDateFormat(date))
-
-        // const userToInsert: InsertUser = {
-        //     username: this.form.get('username')?.value?.trim() || '',
-        //     password: this.form.get('password')?.value?.trim() || '',
-        //     role: isAdmin ? "ADMIN" : "USER"
-        // };
+        const friendToInsert: FriendInsert = {
+            firstname: this.form.get('firstname')?.value?.trim() || '',
+            lastname: this.form.get('lastname')?.value?.trim() || '',
+            nickname: this.form.get('nickname')?.value?.trim(),
+            dateOfBirth: this.createValidDateFormat(date)
+        };
         
-        // console.log(userToInsert);
+        console.log("Friend sent to backend: ", friendToInsert);
         
-        // this.userService.registerUser(userToInsert).subscribe({
-        //     next: (response) => {
-        //         console.log("Response from backend: ", response)
-        //         this.successfulInsertion = true;
-        //         this.insertedFullname = userToInsert.username;
-        //     },
-        //     error: (error) => {
-        //         console.log('Error from Backend', error)
-        //         const errorResponse = error.error as ErrorResponse;
-        //         console.log('Error response', errorResponse);
+        this.friendService.insertFriend(friendToInsert).subscribe({
+            next: (response) => {
+                console.log("Response from backend: ", response)
+                this.successfulInsertion = true;
+                this.insertedFullname = `${response.firstname} ${response.lastname}`;
+                this.friendAlreadyExists = false;
 
-        //         this.friendAlreadyExists = true;
-        //         this.insertedFullname = userToInsert.username;
-        //     }
-        // });
+                this.cdr.detectChanges(); // notifying Angular of the changes
+            },
+            error: (error) => {
+                console.log('Error from Backend', error)
+                const errorResponse = error.error as ErrorResponse;
+                console.log('Error response', errorResponse);
+
+                this.friendAlreadyExists = true;
+                this.successfulInsertion = false;
+                this.insertedFullname = `${friendToInsert.firstname} ${friendToInsert.lastname}`;
+
+                this.cdr.detectChanges(); // notifying Angular of the changes
+            }
+        });
     }
 
     goBackToMain() {
         this.form.reset();
-        this.router.navigate(['login']);
+        this.router.navigate(['main']);
     }
 }
